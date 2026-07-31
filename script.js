64c5f884c0294491a6f1f3ddc2fce62535fb2bf2
Nicole <nicole.zhang11409@gmail.com>
2026-07-31 12:05:29 -0400

    fixed css styling

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
 let password1 = ""
 let password2 = ""
 let password1El = document.getElementById("password-one-el")
 let password2El = document.getElementById("password-two-el")
+let passwordLength = 15
+
+function getRandomCharacter() {
+     let randomChar = characters[Math.floor(Math.random() * characters.length)]
+     return randomChar
+}
 
 function generatePasswords() {
+    password1 = ""
+    password2 = ""
+    for (let i = 0; i <= passwordLength; i++) {
+        password1 += getRandomCharacter()
+        password2 += getRandomCharacter()
+    }
     changeText()
 }
 
